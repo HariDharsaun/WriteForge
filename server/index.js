fa175fc606f4e12ef098486af5889de5825a42c8
HariDharsaun <haridharsaun18personal@gmail.com>
2025-11-02 21:21:14 +0530

    last commit

--- server/index.js
+++ server/index.js
@@ -7,7 +7,26 @@ const generateRoutes = require('./routes/generate');
 const postRoutes = require('./routes/posts');
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:4000',
+  'write-forge-ten.vercel.app',
+  'https://writeforge-ruf6.onrender.com'
+]
+
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin) return callback(null, true); // allow server-to-server or curl
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = `CORS policy: Origin ${origin} not allowed`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true, // allow cookies/auth headers
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
